aae6cf6d2b589038080f585a463ccdaece35bc9e
Paulo Fernandes <phfernandespereira@gmail.com>
2022-04-20 19:03:14 -0300

    ajusta tema de botao disabled

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ export const Button: React.FC<InputHTMLAttributes<HTMLButtonElement>> = (props) 
   <button
     onClick={props.onClick}
     disabled={props.disabled}
-    className="my-5 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    className="my-5 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-indigo-500 disabled:cursor-not-allowed"
   >
     { props.children }
   </button>
